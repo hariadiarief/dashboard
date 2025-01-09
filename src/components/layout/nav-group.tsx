e8d42ce91b95abc19e7b0436474c1197e43d3c21
hariadiarief <adi.arief77@gmail.com>
2025-01-09 21:52:50 +0700

    fix bug menu

--- src/components/layout/nav-group.tsx
+++ src/components/layout/nav-group.tsx
@@ -180,8 +180,6 @@ function checkIsActive(href: string, item: NavItem, mainNav = false) {
     href === item.url || // /endpint?search=param
     href.split('?')[0] === item.url || // endpoint
     !!item?.items?.filter(i => i.url === href).length || // if child nav is active
-    (mainNav &&
-      href.split('/')[1] !== '' &&
-      href.split('/')[1] === item?.url?.split('/')[1])
+    (mainNav && href.split('/')[1] !== '')
   )
 }
